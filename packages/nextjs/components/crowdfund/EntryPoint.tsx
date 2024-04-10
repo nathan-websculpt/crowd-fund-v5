e398d711161bcc503fcf8b74bc167d773b8a2c80
nathan-websculpt <websculpt.nathan@gmail.com>
2024-04-10 09:12:35 -0500

    going to vercel to be sure this works in the wild

--- packages/nextjs/components/crowdfund/EntryPoint.tsx
+++ packages/nextjs/components/crowdfund/EntryPoint.tsx
@@ -11,41 +11,18 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
  */
 export const EntryPoint = () => {
   return (
-    <ConnectButton.Custom>
-      {({ account, chain, openConnectModal, mounted }) => {
-        const connected = mounted && account && chain;
+    <>
+      <Link href="/crowdfund/start-fund-run" passHref className="m-2 btn btn-primary">
+        <div className="tooltip tooltip-primary" data-tip="Start your Fund Run today!">
+          Start Fund Run
+        </div>
+      </Link>
 
-        return (
-          <>
-            {(() => {
-              if (!connected) {
-                return (
-                  <button className="btn btn-primary btn-sm" onClick={openConnectModal} type="button">
-                    Connect Wallet
-                  </button>
-                );
-              }
-
-              //entry to dapp
-              return (
-                <>
-                  <Link href="/crowdfund/start-fund-run" passHref className="m-2 btn btn-primary">
-                    <div className="tooltip tooltip-primary" data-tip="Start your Fund Run today!">
-                      Start Fund Run
-                    </div>
-                  </Link>
-
-                  <Link href="/crowdfund/browse-fund-runs" passHref className="m-2 btn btn-primary">
-                    <div className="tooltip tooltip-primary" data-tip="Donate to projects">
-                      Start Donating
-                    </div>
-                  </Link>
-                </>
-              );
-            })()}
-          </>
-        );
-      }}
-    </ConnectButton.Custom>
+      <Link href="/crowdfund/browse-fund-runs" passHref className="m-2 btn btn-primary">
+        <div className="tooltip tooltip-primary" data-tip="Donate to projects">
+          Start Donating
+        </div>
+      </Link>
+    </>
   );
 };
